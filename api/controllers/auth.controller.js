2efc858ec93d802879a459459e3cf551fb0687d7
MohitGupta <krishgupta1630@gmail.com>
2023-10-02 12:57:59 +0530

    fix sign-up api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,29 @@
 import User from "../models/User.js";
+import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  // hashing password
-  const hashPassword = await bcrypt.hash(password, 10);
-
-  // create a new user
-  const newUser = new User({
-    username,
-    email,
-    password: hashPassword,
-  });
-
   try {
+    // Check if user already exists
+    const isUser = await User.findOne({ email });
+
+    if (isUser) {
+      return next(errorHandler(403, "Username or Email already exists"));
+    }
+
+    // hashing password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    // save user
     await newUser.save();
     res.status(201).json({
       success: true,
